1717f837e7eb2f76a1f776e8100666b9a27b3703
IliketoTranslate <96842479+IliketoTranslate@users.noreply.github.com>
2022-01-24 15:00:27 +0100

    Update game.py

--- game.py
+++ game.py
@@ -11,8 +11,11 @@ def draw_hitbox(object) :
     pygame.draw.rect(window, (93, 32, 32), object)
 
 # zmienne
-game_version = 0.6
-last_update = 23.01
+wytrzymałość_kilofa = 50
+max_wytrzymałość_kilofa = 50
+dodaj2 = 1
+game_version = 0.7
+last_update = 24.01
 x_for_kilof = 400
 y_for_kilof = 400
 x_for_button1 = 1150
@@ -20,21 +23,26 @@ y_for_button1 = 100
 boost = 1
 doswiadczenie = 0
 dodaj = 1
+max_dodaj = 1
 kilof_upgrade = 50
+choosed_kilof = 1
 
 # obiekty
-kilof = pygame.image.load("Kilof.png")
+kilof = pygame.image.load("Drewniany_kilof.png")
+kilof2 = pygame.image.load("Kamienny_kilof.png")
+kilof3 = pygame.image.load("Zelazny_kilof.png")
+kilof4 = pygame.image.load("Zloty_kilof.png")
+kilof5 = pygame.image.load("Diamentowy_kilof.png")
 button_upgrade = pygame.image.load("Button_upgrade.png")
 button_upgrade_clicked = pygame.image.load("Button_upgrade_clicked.png")
-#button_upgrade2 = pygame.image.load("Button_upgrade2.png")
-#button_upgrade2_clicked = pygame.image.load("Button_upgrade2_clicked.png")
+button_upgrade2 = pygame.image.load("Button_upgrade2.png")
+button_upgrade2_clicked = pygame.image.load("Button_upgrade2_clicked.png")
 tlo = pygame.image.load("tlo.png")
 
-
 # hitboxy
-kilof_hitbox = pygame.rect.Rect(x_for_kilof, y_for_kilof, 330, 300)  # tworzy hitbox do kilofa
+kilof_hitbox = pygame.rect.Rect(x_for_kilof, y_for_kilof, 160, 160)  # tworzy hitbox do kilofa
 button_upgrade_hitbox = pygame.rect.Rect(1150, 100, 650, 100)   # tworzy hitbox do przycisku
-#button_upgrade2_hitbox = pygame.rect.Rect(1160, 880, 550, 100)
+button_upgrade2_hitbox = pygame.rect.Rect(1150, 880, 550, 100)
 
 run = True
 
@@ -44,49 +52,83 @@ while run:
         if event.type == pygame.QUIT:  # jeśli gracz zamknie okienko
             run = False
 
-    text_wersja = pygame.font.Font.render(pygame.font.SysFont("Freemono", 50), f"Version : {game_version} | Last update : {last_update}", True, (255, 200, 100)) # generowanie tekstu 3
+    text_wersja = pygame.font.Font.render(pygame.font.SysFont("Freemono", 50), f"Version : {game_version} | Last update : {last_update}", True, (255, 200, 100)) # generowanie tekstu 
     text_doswiadczenie = pygame.font.Font.render(pygame.font.SysFont("Dyuthi", 72), f"Doswiadczenie : {doswiadczenie}", True, (100, 100, 100)) # generowanie tekstu
-    #text_kilof = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Kup kilof | Koszt : {kilof_upgrade}", True, (255, 255, 255)) # generowanie tekstu 2
-    if doswiadczenie > boost :
-        text_ulepszenie = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Ulepszenie kilofa | Koszt : {boost}, Dostepne", True, (255, 255, 255)) # generowanie tekstu 2
+    text_kilof = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Kup kilof | Koszt : {kilof_upgrade}", True, (255, 255, 255)) # generowanie tekstu 
+    text_WIP = pygame.font.Font.render(pygame.font.SysFont("Waree", 25), f"W I P (WORK IN PROGRESS)", True, (255, 255, 255)) # generowanie tekstu 2
+    text_wytrzymałość_kilofa = pygame.font.Font.render(pygame.font.SysFont("Dyuthi", 50), f"Wytrzymalosc kilofa : {wytrzymałość_kilofa}", True, (255, 255, 255)) # generowanie tekstu 2
+    if doswiadczenie > kilof_upgrade :
+        text_kilof = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Kup kilof | Koszt : {kilof_upgrade}, Dostepne", True, (255, 255, 255)) # generowanie tekstu 2
     else :
-        text_ulepszenie = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Ulepszenie kilofa | Koszt : {boost}, Niedostepne", True, (255, 255, 255)) # generowanie tekstu 2
-
-    #if doswiadczenie > kilof_upgrade :
-        #text_kilof = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Kup kilof | Koszt : {kilof_upgrade}, Dostepne", True, (255, 255, 255)) # generowanie tekstu 2
-    #else :
-        #text_kilof = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Kup kilof | Koszt : {kilof_upgrade}, Niedostepne", True, (255, 255, 255)) # generowanie tekstu 2
-
+        text_kilof = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Kup kilof | Koszt : {kilof_upgrade}, Niedostepne", True, (255, 255, 255)) # generowanie tekstu 2
+    
+    boost2 = boost - 1
+    
+    if doswiadczenie > boost2 :
+        text_ulepszenie = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Ulepszenie kilofa | Koszt : {boost}, Dostepne", True, (255, 255, 255)) # generowanie tekstu 
+    else :
+        text_ulepszenie = pygame.font.Font.render(pygame.font.SysFont("Sawasdee", 25), f"Ulepszenie kilofa | Koszt : {boost}, Niedostepne", True, (255, 255, 255)) # generowanie tekstu 
+    
     window.blit(tlo, (0, 0))  # rysowanie tła
 
     # rysowanie hitboxów
     draw_hitbox(kilof_hitbox) # rysowanie hitboxu do kilofa
     draw_hitbox(button_upgrade_hitbox) # rysowanie hitboxu do przycisku upgrade
-    #draw_hitbox(button_upgrade2_hitbox) # rysowanie hitboxu do przycisku shop
+    draw_hitbox(button_upgrade2_hitbox) # rysowanie hitboxu do przycisku shop
         
     # rysowanie obiektów
-        
-    draw_object(kilof, x_for_kilof, y_for_kilof) # rysowanie kilofu
+     
+    if choosed_kilof == 1 : draw_object(kilof, x_for_kilof, y_for_kilof)  # rysowanie kilofu
+    elif choosed_kilof == 2 : draw_object(kilof2, x_for_kilof, y_for_kilof)
+    elif choosed_kilof == 3 : draw_object(kilof3, x_for_kilof, y_for_kilof)
+    elif choosed_kilof == 4 : draw_object(kilof4, x_for_kilof, y_for_kilof)
+    elif choosed_kilof == 5 or choosed_kilof > 5 : draw_object(kilof5, x_for_kilof, y_for_kilof)
     draw_object(button_upgrade, x_for_button1, y_for_button1) # rysowanie przycisku
-    #draw_object(button_upgrade2, 1160, 880) # rysowanie przycisku 2
+    draw_object(button_upgrade2, 1150, 880) # rysowanie przycisku 2
     draw_object(text_doswiadczenie, 224, 100) # rysowanie tekstu
     draw_object(text_ulepszenie, 1160, 135) # rysowanie tekstu 2
     draw_object(text_wersja, 10, 5) # rysowanie tekstu 3
-    #draw_object(text_kilof, 1165, 920)
-        
+    draw_object(text_kilof, 1165, 920)
+    draw_object(text_WIP, 1200, 820)
+    draw_object(text_wytrzymałość_kilofa, 250, 300)
 
-    # sprawdzanie zdarzeń z myszką
+    # sprawdzanie zdarzeń z myszką                  
+    if wytrzymałość_kilofa == 0 :                                                                                                                           ## BŁĄD ##
+        choosed_kilof = choosed_kilof - 1                                                                                                                   ## BŁĄD ##
+        dodaj = 0                                                                                                                                           ## BŁĄD ##
+        dodaj2 = 0                                                                                                                                          ## BŁĄD ##
+    else :                                                                                                                                                  ## BŁĄD ##
+        dodaj = max_dodaj                                                                                                                                   ## BŁĄD ##
+        dodaj2 = 1                                                                                                                                          ## BŁĄD ##
+        if button_upgrade2_hitbox.collidepoint(pygame.mouse.get_pos()) and doswiadczenie > kilof_upgrade : # jeżeli mysz dotyka hitboxa                     ## BŁĄD ##   
+            if pygame.mouse.get_pressed()[0]:                                                              # jeżeli naciśnieto lewy przycisk myszy          ## BŁĄD ##
+                doswiadczenie = doswiadczenie - kilof_upgrade                                                                                               ## BŁĄD ##
+                choosed_kilof += 1                                                                                                                          ## BŁĄD ##
+                kilof_upgrade = kilof_upgrade * 2                                                                                                           ## BŁĄD ##
+                max_wytrzymałość_kilofa = max_wytrzymałość_kilofa * 2                                                                                       ## BŁĄD ##
+                wytrzymałość_kilofa = max_wytrzymałość_kilofa                                                                                               ## BŁĄD ##
+                                                                                                                                                            ## BŁĄD ##
     if kilof_hitbox.collidepoint(pygame.mouse.get_pos()):
         if pygame.mouse.get_pressed()[0]:
             pygame.time.wait(100)
             doswiadczenie += dodaj
-        
-    if button_upgrade_hitbox.collidepoint(pygame.mouse.get_pos()) and doswiadczenie > boost :
+            wytrzymałość_kilofa = wytrzymałość_kilofa - dodaj2
+
+    boost2 = boost - 1
+
+    if button_upgrade_hitbox.collidepoint(pygame.mouse.get_pos()) and doswiadczenie >  boost2:
         if pygame.mouse.get_pressed()[0]:
-            dodaj += 1
+            max_dodaj += 1
             doswiadczenie = doswiadczenie - boost
             boost = boost * 2
             pygame.time.wait(100)
+
+    if button_upgrade2_hitbox.collidepoint(pygame.mouse.get_pos()):
+        draw_object(button_upgrade2_clicked, 1150, 880) # rysowanie przycisku
+        draw_object(text_kilof, 1165, 920) # rysowanie tekstu 2
+    else :
+        draw_object(button_upgrade2, 1150, 880) # rysowanie przycisku
+        draw_object(text_kilof, 1165, 920) # rysowanie tekstu 2
 
     if button_upgrade_hitbox.collidepoint(pygame.mouse.get_pos()):
         draw_object(button_upgrade_clicked, x_for_button1, y_for_button1) # rysowanie przycisku
@@ -94,13 +136,6 @@ while run:
     else :
         draw_object(button_upgrade, x_for_button1, y_for_button1) # rysowanie przycisku
         draw_object(text_ulepszenie, 1160, 135) # rysowanie tekstu 2
-
-    #if button_upgrade2_hitbox.collidepoint(pygame.mouse.get_pos()):
-        #draw_object(button_upgrade2_clicked, 1160, 880) # rysowanie przycisku
-        #draw_object(text_kilof, 1165, 920) # rysowanie tekstu 2
-    #else :
-        #draw_object(button_upgrade2, 1160, 880) # rysowanie przycisku
-        #draw_object(text_kilof, 1165, 920) # rysowanie tekstu 2
 
     # wydrukuj
     pygame.display.update()
